0c5324d7f63f315f9caef1b1d0c38b30e4cf7809
Alessandro <alessandro.portosa@eng.it>
2017-09-21 17:54:36 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ if [[ -z "$PUBLIC_ADDRESS" ]]; then
         #get the address of container
         #example : default via 172.17.42.1 dev eth0 172.17.0.0/16 dev eth0 proto kernel scope link src 172.17.0.109
         PUBLIC_ADDRESS=`ip route | grep src | awk '{print $9}'`
-
-        #replace the address of container inside server.xml
-        sed -i "s/http:\/\/.*:8080/http:\/\/${PUBLIC_ADDRESS}:8080/g" ${KNOWAGE_DIRECTORY}/conf/server.xml
-        sed -i "s/http:\/\/localhost:8080/http:\/\/${PUBLIC_ADDRESS}:8080/g" ${KNOWAGE_DIRECTORY}/webapps/knowage/WEB-INF/web.xml
 fi
+
+#replace the address of container inside server.xml
+sed -i "s/http:\/\/.*:8080/http:\/\/${PUBLIC_ADDRESS}:8080/g" ${KNOWAGE_DIRECTORY}/conf/server.xml
+sed -i "s/http:\/\/localhost:8080/http:\/\/${PUBLIC_ADDRESS}:8080/g" ${KNOWAGE_DIRECTORY}/webapps/knowage/WEB-INF/web.xml
 
 #wait for MySql if it's a compose image
 if [ -n "$WAIT_MYSQL" ]; then
