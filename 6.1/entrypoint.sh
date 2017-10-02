b81c6fb0f87be88826ce251c54abd9536b80c5df
Alessandro <alessandro.portosa@eng.it>
2017-10-02 23:59:34 +0200

    Update entrypoint.sh

--- 6.1/entrypoint.sh
+++ 6.1/entrypoint.sh
@@ -8,8 +8,8 @@ if [[ -z "$PUBLIC_ADDRESS" ]]; then
 fi
 
 #replace the address of container inside server.xml
-sed -i "s|http:\/\/.*:8080|http:\/\/${PUBLIC_ADDRESS}:8080|g" ${KNOWAGE_DIRECTORY}/${TOMCAT_DIRECTORY}/conf/server.xml
-sed -i "s|http:\/\/localhost:8080|http:\/\/${PUBLIC_ADDRESS}:8080|g" ${KNOWAGE_DIRECTORY}/${TOMCAT_DIRECTORY}/webapps/knowage/WEB-INF/web.xml
+sed -i "s|http:\/\/.*:8080|http:\/\/${PUBLIC_ADDRESS}:8080|g" ${KNOWAGE_DIRECTORY}/${APACHE_TOMCAT_PACKAGE}/conf/server.xml
+sed -i "s|http:\/\/localhost:8080|http:\/\/${PUBLIC_ADDRESS}:8080|g" ${KNOWAGE_DIRECTORY}/${APACHE_TOMCAT_PACKAGE}/webapps/knowage/WEB-INF/web.xml
 
 #wait for mysql if it's a compose image
 if [ -n "$WAIT_MYSQL" ]; then
@@ -30,13 +30,13 @@ DB_PORT=$DB_PORT_3306_TCP_PORT
 #insert knowage metadata into db if it doesn't exist
 result=`mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} -e "SHOW TABLES LIKE '%SBI_%';"`
 if [ -z "$result" ]; then
-	mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} --execute="source $MYSQL_SCRIPT_DIRECTORY/MySQL_create.sql"
-        mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} --execute="source $MYSQL_SCRIPT_DIRECTORY/MySQL_create_quartz_schema.sql"
+	mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} --execute="source ${MYSQL_SCRIPT_DIRECTORY}/MySQL_create.sql"
+        mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} --execute="source ${MYSQL_SCRIPT_DIRECTORY}/MySQL_create_quartz_schema.sql"
 fi
 
 #replace in server.xml
 old_connection='url="jdbc:mysql://localhost:3306/knowagedb" username="knowageuser" password="knowagepassword"'
 new_connection='url="jdbc:mysql://'${DB_HOST}':'${DB_PORT}'/'${DB_DB}'" username="'${DB_USER}'" password="'${DB_PASS}'"'
-sed -i "s|${old_connection}|${mysql_connection}|" ${KNOWAGE_DIRECTORY}/${TOMCAT_DIRECTORY}/conf/server.xml
+sed -i "s|${old_connection}|${mysql_connection}|" ${KNOWAGE_DIRECTORY}/${APACHE_TOMCAT_PACKAGE}/conf/server.xml
 
 exec "$@"
