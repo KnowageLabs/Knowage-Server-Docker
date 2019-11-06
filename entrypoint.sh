75f62c09dfb0ebf1f820be5e97ccae64b9ab9f57
Marco Libanori <marco.libanori@eng.it>
2019-11-06 11:33:55 -0500

    Trying secrets in swarm mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 set -e
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env "DB_USER"
+file_env "DB_PASS"
+file_env "DB_DB"
+file_env "DB_HOST"
+file_env "DB_PORT"
+
 if [[ -z "$PUBLIC_ADDRESS" ]]; then
         #get the address of container
         #example : default via 172.17.42.1 dev eth0 172.17.0.0/16 dev eth0 proto kernel scope link src 172.17.0.109
@@ -33,3 +61,4 @@ echo "The HMAC key will be: ${hmac_key}"
 sed -i "s|abc123|${hmac_key}|" ${KNOWAGE_DIRECTORY}/${APACHE_TOMCAT_PACKAGE}/conf/server.xml
 
 exec "$@"
+
