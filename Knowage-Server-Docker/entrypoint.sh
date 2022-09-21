9d41d5f0e9c110ee52bed69d7c362a243a316cab
Marco Libanori <marco.libanori@eng.it>
2022-09-21 18:39:56 +0200

    Lot of improvements
    - The JNDI initialization can manage MySQL, MariaDB, Postgres and Oracle
    - Fix TLS certificate and key
    - We can avoid DB initialization
    - Fixed image version in docker-*.yaml
    - Some refactoring
    - The image brings all the SQL scripts for every DB
    - The DB initializations is still available for MySQL and MariaDB

--- Knowage-Server-Docker/entrypoint.sh
+++ Knowage-Server-Docker/entrypoint.sh
@@ -23,6 +23,8 @@ file_env() {
 	unset "$fileVar"
 }
 
+file_env "DB_DO_INITIALIZATION" "true"
+
 file_env "DB_HOST"
 file_env "DB_PORT"
 file_env "DB_DB"
@@ -31,6 +33,7 @@ file_env "DB_PASS"
 file_env "DB_CONNECTION_POOL_SIZE"   "20"
 file_env "DB_CONNECTION_MAX_IDLE"    "10"
 file_env "DB_CONNECTION_WAIT_MS"     "300"
+file_env "DB_TYPE"                   "MYSQL"
 
 file_env "CACHE_DB_HOST"
 file_env "CACHE_DB_PORT"
@@ -40,6 +43,7 @@ file_env "CACHE_DB_PASS"
 file_env "CACHE_DB_CONNECTION_POOL_SIZE"   "20"
 file_env "CACHE_DB_CONNECTION_MAX_IDLE"    "10"
 file_env "CACHE_DB_CONNECTION_WAIT_MS"     "300"
+file_env "CACHE_DB_TYPE"                   "MYSQL"
 
 file_env "AJP_SECRET"
 
@@ -53,7 +57,7 @@ then
 fi
 
 # Wait for MySql
-./wait-for-it.sh ${DB_HOST}:${DB_PORT}
+./scripts/wait-for-it.sh -t 60 ${DB_HOST}:${DB_PORT}
 WAIT_RET_CODE=$?
 
 if [ $WAIT_RET_CODE -eq 0 ]
@@ -67,7 +71,7 @@ then
 		file_env "HMAC_KEY"
 		file_env "PASSWORD_ENCRYPTION_SECRET"
 		file_env "PUBLIC_ADDRESS"
-	
+		
 		# Generate default values for the optional env vars
 		if [[ -z "$PUBLIC_ADDRESS" ]]
 		then
@@ -93,13 +97,13 @@ then
 			echo "The DB_DB, DB_USER, DB_PASS environment variables are needed"
 			exit -1
 		fi
-	
+		
 		if [ -z "$CACHE_DB_HOST" ] || [ -z "$CACHE_DB_PORT" ] || [ -z "$CACHE_DB_DB"   ] || [ -z "$CACHE_DB_USER" ] || [ -z "$CACHE_DB_PASS" ]
 		then
 			echo "The CACHE_DB_HOST, CACHE_DB_PORT, CACHE_DB_DB, CACHE_DB_USER, CACHE_DB_PASS environment variables are needed"
 			exit -1
 		fi
-	
+		
 		if [ -z "$AJP_SECRET" ]
 		then
 			AJP_SECRET=$( openssl rand -base64 32 )
@@ -109,7 +113,7 @@ then
 			echo "#   ${AJP_SECRET}"
 			echo "#"
 		fi
-	
+		
 		if [ -z "$HAZELCAST_HOSTS" ]
 		then
 			HAZELCAST_HOSTS="127.0.0.1"
@@ -119,44 +123,70 @@ then
 			echo "# one internally."
 			echo "#"
 		fi
-	
+		
 		if [ -z "$HAZELCAST_PORT" ]
 		then
 			HAZELCAST_PORT="5701"
 		fi
-	
+		
 		# Replace the address of container inside server.xml
 		sed -i "s|http:\/\/.*:8080|http:\/\/${PUBLIC_ADDRESS}:8080|g" ${KNOWAGE_DIRECTORY}/apache-tomcat/conf/server.xml
 		sed -i "s|http:\/\/.*:8080\/knowage|http:\/\/localhost:8080\/knowage|g" ${KNOWAGE_DIRECTORY}/apache-tomcat/conf/server.xml
 		sed -i "s|http:\/\/localhost:8080|http:\/\/${PUBLIC_ADDRESS}:8080|g" ${KNOWAGE_DIRECTORY}/apache-tomcat/webapps/knowage/WEB-INF/web.xml
 		
-		# Insert knowage metadata into db if it doesn't exist
-		result=`mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} -e "SHOW TABLES LIKE '%SBI_%';"`
-		if [ -z "$result" ]; then
-			mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} --execute="source ${MYSQL_SCRIPT_DIRECTORY}/MySQL_create.sql"
-			mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} --execute="source ${MYSQL_SCRIPT_DIRECTORY}/MySQL_create_quartz_schema.sql"
+		if [ $DB_DO_INITIALIZATION == "true" ]; then
+			if [ $DB_TYPE == "MYSQL" ] || [ $DB_TYPE.==."MARIADB" ]; then
+				# Insert knowage metadata into db if it doesn't exist
+				result=`mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} -e "SHOW TABLES LIKE '%SBI_%';"`
+				if [ -z "$result" ]; then
+					mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} --execute="source ${MYSQL_SCRIPT_DIRECTORY}/MySQL_create.sql"
+					mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} ${DB_DB} --execute="source ${MYSQL_SCRIPT_DIRECTORY}/MySQL_create_quartz_schema.sql"
+				fi
+			else
+				echo "WARNING: Initialization is only available for MySQL/MariaDB"
+			fi
+		fi
+		
+		JNDI_DB_SCRIPT_DISCRIMINATOR=$( echo $DB_TYPE | tr '[:upper:]' '[:lower:]' )
+		JNDI_CACHE_SCRIPT_DISCRIMINATOR=$( echo $CACHE_DB_TYPE | tr '[:upper:]' '[:lower:]' )
+		
+		JNDI_DB_SCRIPT=./scripts/set-jndi-resources-for-${JNDI_DB_SCRIPT_DISCRIMINATOR}-db.sh
+		JNDI_CACHE_SCRIPT=./scripts/set-jndi-resources-for-${JNDI_CACHE_SCRIPT_DISCRIMINATOR}-cache.sh
+		
+		if [ ! -f $JNDI_DB_SCRIPT ]; then
+			echo "ERROR: The script $JNDI_DB_SCRIPT for DB type $DB_TYPE doesn't exist"
+			exit -2
+		fi
+		
+		if [ ! -f $JNDI_CACHE_SCRIPT ]; then
+			echo "ERROR: The script $JNDI_CACHE_SCRIPT for cache DB type $CACHE_DB_TYPE doesn't exist"
+			exit -2
 		fi
 		
 		# Set DB connection for Knowage metadata
-		./scripts/set-jndi-resources-for-db.sh "${DB_HOST}" \
-                        "${DB_PORT}" \
-                        "${DB_DB}" \
-                        "${DB_USER}" \
-                        "${DB_PASS}" \
-                        "${DB_CONNECTION_POOL_SIZE}" \
-                        "${DB_CONNECTION_MAX_IDLE}" \
-                        "${DB_CONNECTION_WAIT_MS}"
-
+		#./scripts/set-jndi-resources-for-db.sh "${DB_HOST}" \
+		#	"${DB_PORT}" \
+		#	"${DB_DB}" \
+		#	"${DB_USER}" \
+		#	"${DB_PASS}" \
+		#	"${DB_CONNECTION_POOL_SIZE}" \
+		#	"${DB_CONNECTION_MAX_IDLE}" \
+		#	"${DB_CONNECTION_WAIT_MS}"
+		JNDI_DB_CMD=( "$JNDI_DB_SCRIPT" "${DB_HOST}" "${DB_PORT}" "${DB_DB}" "${DB_USER}" "${DB_PASS}" "${DB_CONNECTION_POOL_SIZE}" "${DB_CONNECTION_MAX_IDLE}" "${DB_CONNECTION_WAIT_MS}" )
+		"${JNDI_DB_CMD[@]}"
+		
 		# Set DB connection for Knowage cache
-		./scripts/set-jndi-resources-for-cache.sh "${CACHE_DB_HOST}" \
-                        "${CACHE_DB_PORT}" \
-                        "${CACHE_DB_DB}" \
-                        "${CACHE_DB_USER}" \
-                        "${CACHE_DB_PASS}" \
-                        "${CACHE_DB_CONNECTION_POOL_SIZE}" \
-                        "${CACHE_DB_CONNECTION_MAX_IDLE}" \
-                        "${CACHE_DB_CONNECTION_WAIT_MS}"
-
+		#./scripts/set-jndi-resources-for-cache.sh "${CACHE_DB_HOST}" \
+		#	"${CACHE_DB_PORT}" \
+		#	"${CACHE_DB_DB}" \
+		#	"${CACHE_DB_USER}" \
+		#	"${CACHE_DB_PASS}" \
+		#	"${CACHE_DB_CONNECTION_POOL_SIZE}" \
+		#	"${CACHE_DB_CONNECTION_MAX_IDLE}" \
+		#	"${CACHE_DB_CONNECTION_WAIT_MS}"
+		JNDI_CACHE_DB_CMD=( "$JNDI_CACHE_SCRIPT" "${CACHE_DB_HOST}" "${CACHE_DB_PORT}" "${CACHE_DB_DB}" "${CACHE_DB_USER}" "${CACHE_DB_PASS}" "${CACHE_DB_CONNECTION_POOL_SIZE}" "${CACHE_DB_CONNECTION_MAX_IDLE}" "${CACHE_DB_CONNECTION_WAIT_MS}" )
+		"${JNDI_CACHE_DB_CMD[@]}"
+		
 		# Set HMAC key
 		xmlstarlet ed -P -L \
 			-u "//Server/GlobalNamingResources/Environment[@name='hmacKey']/@value" -v "${HMAC_KEY}" \
@@ -164,7 +194,7 @@ then
 		
 		# Set password encryption key
 		echo $PASSWORD_ENCRYPTION_SECRET > ${KNOWAGE_DIRECTORY}/apache-tomcat/conf/passwordEncryptionSecret
-	
+		
 		# Set AJP secret
 		xmlstarlet ed -P -L \
 			-d "//Server/Service/Connector[contains(@protocol, 'AJP')]/@secretRequired" \
@@ -182,12 +212,12 @@ then
 		# Setting hazelcast.xml
 		# 
 		# N.B.: the _ in Xmlstarlet XPath stands for default namespace
-	
+		
 		# Set port
 		xmlstarlet ed -P -L \
 			-u "/_:hazelcast/_:network/_:port" -v ${HAZELCAST_PORT} \
 			${KNOWAGE_DIRECTORY}/apache-tomcat/conf/hazelcast.xml
-	
+		
 		# Clean up the member list
 		xmlstarlet ed -P -L \
 			-d "/_:hazelcast/_:network/_:join/_:tcp-ip/_:member-list/_:member" \
@@ -198,10 +228,10 @@ then
 			xmlstarlet ed -P -L \
 				-s "/_:hazelcast/_:network/_:join/_:tcp-ip/_:member-list" -t elem -n member -v \{\} \
 				${KNOWAGE_DIRECTORY}/apache-tomcat/conf/hazelcast.xml
-	
+		
 		# Format
 		xmlstarlet ed -O -L ${KNOWAGE_DIRECTORY}/apache-tomcat/conf/hazelcast.xml
-	
+		
 		# Create the placeholder to prevent multiple initializations
 		touch "$CONTAINER_INITIALIZED_PLACEHOLDER"
 	fi
